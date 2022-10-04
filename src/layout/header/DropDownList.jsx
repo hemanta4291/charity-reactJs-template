43a8502cc40cdb0bb04239bed8fe7ae74b46864d
hemantaroy <hemanta@orangetoolz.com>
2022-10-04 13:13:03 +0600

    droudown paddding change

--- src/layout/header/DropDownList.jsx
+++ src/layout/header/DropDownList.jsx
@@ -5,8 +5,9 @@ const DropDownList = ({closeToggleSlide,dropDownArray}) => {
 
     let menuListRendarView = null;
     menuListRendarView = dropDownArray?.map((item)=>(
-        <li className='text-gray-800 hover:text-primary font-semibold px-4 py-2' key={item.id} onClick={closeToggleSlide}>
+        <li className='text-gray-800 hover:text-primary font-semibold' key={item.id} onClick={closeToggleSlide}>
             <NavLink
+                className="px-4 py-1.5 inline-block"
                 to={item.to}>
                 {item.title}
             </NavLink>
